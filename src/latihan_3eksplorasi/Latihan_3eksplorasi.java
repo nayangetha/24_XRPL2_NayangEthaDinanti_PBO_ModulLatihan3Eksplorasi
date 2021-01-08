c5aa509bb87a6166a3c0796acf2e1b9380d7e287
unknown <nayang_etha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-08 18:52:43 +0700

    first commit

---
+++ src/latihan_3eksplorasi/Latihan_3eksplorasi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_3eksplorasi;
+
+/**
+ *
+ * @author MOKLET1
+ */
+public class Latihan_3eksplorasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //deklarasi variabel
+        double bil_a, bil_b,hasil;
+        //menyimpan data dalam variabel
+        bil_a=454.55;
+        bil_b=546.55;
+        hasil = bil_a+bil_b;
+        //Memanggil variabel untuk ditampilkan
+        System.out.print("Hasil Penjumlahan kedua bilangan : ");
+        System.out.println(hasil);
+    }
+    
+}
